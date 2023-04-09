e9bade095e2b38b3081070d7dc9dc711dacab90a
shylabo <shuya.sohot@gmail.com>
2023-04-08 18:38:00 -0700

    added some projects

--- data.ts
+++ data.ts
@@ -154,4 +154,134 @@ export const projects: IProject[] = [
     category: ["react"],
     key_techs: ["React", "Syncfusion", "TailwindCSS"],
   },
+  {
+    name: "Twitter app",
+    description:
+      "This is a Twitter clone application that enables you to tweet, reply, and engage with other users, utilizing TypeScript, React, and Firebase. Additionally, the application includes Google account authentication to ensure secure access to your account.",
+    image_path: "/images/twitter-app.png",
+    deployed_url: "/",
+    github_url: "https://github.com/shylabo/firebase-react-ts-twitter-app",
+    category: ["typescript", "react"],
+    key_techs: ["TypeScript", "React.js", "firebase", "HTML", "CSS"],
+  },
+  {
+    name: "Video api with jwt & swagger",
+    description:
+      "This is an example API created with Go, featuring JWT authentication for enhanced security and Swagger for simplified documentation.",
+    image_path: "/images/video-api-jwt-swagger.png",
+    deployed_url: "/",
+    github_url: "https://github.com/shylabo/video-api-jwt-swagger",
+    category: ["go"],
+    key_techs: ["Go", "Docker", "Swagger", "JWT"],
+  },
+  {
+    name: "Fire Extinguisher Rental",
+    description:
+      "This is one of the craziest apps in the world but a user-friendly application designed to help you purchase fire extinguishers with ease.",
+    image_path: "/images/fire-extinguisher-rental.png",
+    deployed_url: "/",
+    github_url: "https://github.com/shylabo/fire-extinguisher-rental",
+    category: ["vue"],
+    key_techs: ["Vue.js", "Nuxt.js", "JavaScript"],
+  },
+  {
+    name: "Shuya Life Web",
+    description:
+      "This website serves as a platform to showcase an individual or business's collection of work and accomplishments. It offers a visually appealing and organized presentation of projects, products, or services, demonstrating the creator's skills, experience, and capabilities. ",
+    image_path: "/images/shuya-life-web.png",
+    deployed_url: "/",
+    github_url: "https://github.com/shylabo/shuya-life-web",
+    category: ["react"],
+    key_techs: ["React.js", "Next.js", "JavaScript", "TailwindCSS"],
+  },
+  {
+    name: "Dad Jokes",
+    description:
+      "This is a Dad Joke application developed using Nuxt.js and Vue.js, providing you with a hilarious collection of Dad Jokes to brighten up your day.",
+    image_path: "/images/dadjokes-nuxt.png",
+    deployed_url: "/",
+    github_url: "https://github.com/shylabo/dadjokes-nuxt",
+    category: ["vue"],
+    key_techs: ["Vue.js", "Nuxt.js", "JavaScript"],
+  },
+  {
+    name: "Dictionary cli",
+    description:
+      "This is a CLI-based application where you can add a word, its definition, and category to a database.",
+    image_path: "/images/dictionary-cli.png",
+    deployed_url: "/",
+    github_url: "https://github.com/shylabo/dictionary-cli",
+    category: ["go"],
+    key_techs: ["Go", "cobra", "promptui", "sqlite"],
+  },
+  {
+    name: "Golang scraper",
+    description:
+      "This application is designed to perform web scraping operations using Go. With its powerful scraping capabilities, the application allows you to efficiently extract relevant data from websites, which can be used for various purposes such as data analysis and research.",
+    image_path: "/images/golang-scraper.png",
+    deployed_url: "/",
+    github_url: "https://github.com/shylabo/golang-scraper",
+    category: ["go"],
+    key_techs: ["Go", "gocolly"],
+  },
+  {
+    name: "PDF generator",
+    description:
+      "This application enables you to easily generate PDF documents by inputting information into customizable templates. With its flexible features and intuitive design, the application allows you to create professional-looking PDFs tailored to your specific needs and requirements, making the document creation process quick and effortless.",
+    image_path: "/images/pdf-gen.png",
+    deployed_url: "/",
+    github_url: "https://github.com/shylabo/pdf-gen",
+    category: ["go"],
+    key_techs: ["Go", "Maroto"],
+  },
+  {
+    name: "Reaction timer",
+    description:
+      "This is a game that tests your reaction time by measuring how quickly you can respond after a timer starts.",
+    image_path: "/images/reaction-timer.png",
+    deployed_url: "/",
+    github_url: "https://github.com/shylabo/vue-reaction-timer",
+    category: ["vue"],
+    key_techs: ["Vue.js", "HTML", "Javascript"],
+  },
+  {
+    name: "Project kanban board",
+    description:
+      "This is a user-friendly Kanban board application designed for efficient management of your development tasks through intuitive manipulation.",
+    image_path: "/images/project-kanban-board.png",
+    deployed_url: "/",
+    github_url: "https://github.com/shylabo/project-kanban-board",
+    category: ["typescript"],
+    key_techs: ["TypeScript", "HTML", "CSS"],
+  },
+  {
+    name: "TODO app",
+    description:
+      "This is a straightforward To-Do list application developed using React.js and TypeScript.",
+    image_path: "/images/todo-app.png",
+    deployed_url: "/",
+    github_url: "https://github.com/shylabo/react-ts-todoapp",
+    category: ["typescript", "react"],
+    key_techs: ["TypeScript", "React.js", "HTML", "CSS"],
+  },
+  {
+    name: "Quick website",
+    description:
+      "This is a powerful application that enables you to quickly build a website using HUGO, a popular open-source static site generator. With its intuitive interface and customizable features, the application streamlines the website-building process, making it easy for users to create professional-looking sites in a matter of minutes.",
+    image_path: "/images/quick-website.png",
+    deployed_url: "/",
+    github_url: "https://github.com/shylabo/quick-website",
+    category: ["others"],
+    key_techs: ["HTML", "Scss", "HUGO", "TailwindCSS"],
+  },
+  {
+    name: "Matrix",
+    description:
+      "This is an application that allows you to experience the iconic Matrix screen effect, as seen in the movie.",
+    image_path: "/images/matrix.png",
+    deployed_url: "/",
+    github_url: "https://github.com/shylabo/matrix",
+    category: ["others"],
+    key_techs: ["HTML", "JavaScript"],
+  },
 ];
